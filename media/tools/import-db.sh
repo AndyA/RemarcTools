22d5e286a7584fc971552d2d176234da579be828
Andy Armstrong <andy@hexten.net>
2016-04-28 13:21:07 +0100

    Drop collection.

--- media/tools/import-db.sh
+++ media/tools/import-db.sh
@@ -4,7 +4,7 @@ db=remarc
 dir="export.$db"
 mkdir -p "$dir"
 for col in audio images theme video; do
-  mongoimport -d "$db" -c "$col" --file "$dir/$col.json"
+  mongoimport -d "$db" -c "$col" --drop --file "$dir/$col.json"
 done
 
 # vim:ts=2:sw=2:sts=2:et:ft=sh
